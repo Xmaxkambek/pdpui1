80edf6a2e220b981532385fef70c1e6939d356c2
Xmaxkambek <maxkambekxolbekov515@gmail.com>
2023-01-26 11:58:07 +0500

    the end

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pdpui1/pages/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
